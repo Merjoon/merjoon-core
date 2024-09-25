00ea19bb6c97203f1fb253b4f0beebc70fc95fad
Levon Gevorgyan <levon.gevorgyan@arctic-fox.tech>
2024-09-25 11:45:13 +0400

    MRJ-71 - Provide verification mechanism

---
+++ src/verify/utils.ts
@@ -0,0 +1,11 @@
+import fs from 'node:fs/promises'
+import { EntityName, IntegrationId } from './types';
+import { IMerjoonEntity } from '../common/types';
+
+export async function saveEntities(serviceName: IntegrationId, entityName: EntityName, payload: IMerjoonEntity[]) {
+  const folder = `.transformed/${serviceName}`
+  await fs.mkdir(folder, {
+    recursive: true,
+  });
+  await fs.writeFile(`${folder}/${entityName}.json`, JSON.stringify(payload, null, 2));
+}
