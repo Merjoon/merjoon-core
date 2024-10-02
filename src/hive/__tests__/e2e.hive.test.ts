ff50f06b330c0bbd85871b9e55ce8b1fcc8d8ded
Maga Harutyunyan <magaharutyunyan969@gmail.com>
2024-10-02 10:30:16 +0400

    MRJ-1 - Hive Integration

--- src/hive/__tests__/e2e.hive.test.ts
+++ src/hive/__tests__/e2e.hive.test.ts
@@ -82,7 +82,7 @@ describe('e2e Hive', () => {
             assignees: expect.arrayContaining([expect.stringMatching(ID_REGEX)]),
             status: expect.any(String),
             description: expect.any(String),
-            projects: expect.arrayContaining([expect.stringMatching(ID_REGEX)]),
+            projects: expect.arrayContaining(tasks[0].projects.length === 0 ? [] : [expect.stringMatching(ID_REGEX)]),
             remote_created_at: expect.any(String),
             remote_modified_at: expect.any(String),
             created_at: expect.any(Number),
--- src/verify/services/hive.ts
+++ src/verify/services/hive.ts
@@ -1,20 +1,3 @@
-import { HiveTransformer } from '../../hive/transformer';
-import { HiveService } from '../../hive/service';
-import { IHiveConfig } from '../../hive/types';
-import { HiveApi } from '../../hive/api';
+import { getHiveService} from "../../hive/hive-service";
 
-const {
-    HIVE_API_KEY,
-    HIVE_USER_ID,
-    HIVE_WORKSPACE_ID,
-} = process.env;
-
-const config: IHiveConfig = {
-    api_key: HIVE_API_KEY!,
-    user_id: HIVE_USER_ID!,
-    workspace_id: HIVE_WORKSPACE_ID!,
-};
-
-const api: HiveApi = new HiveApi(config);
-const transformer: HiveTransformer = new HiveTransformer();
-export const hiveService = new HiveService(api, transformer);
+export const hiveService = getHiveService();
