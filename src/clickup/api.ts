c6d9affe111977c4ac0964b9ea66954a3773d29c
Maga <magaharutyunyan969@gmail.com>
2024-10-20 03:45:24 +0400

    MRJ-7 - ClickUp Integration

--- src/clickup/api.ts
+++ src/clickup/api.ts
@@ -6,7 +6,7 @@ export class ClickUpApi extends HttpClient {
 
   protected readonly apiKey: string;
   
-  protected lists: IClickUpList[] | undefined;
+  // protected lists: IClickUpList[] | undefined;
 
   constructor(protected config: IClickUpConfig) {
     const basePath = `https://api.clickup.com/api/v2`;
@@ -14,24 +14,7 @@ export class ClickUpApi extends HttpClient {
     this.apiKey = config.apiKey;
   }
 
-  // public async init() {
-  //   const config = this.getConfig();
-  //   this.team_ids = await this.fetchIds(`team`, config);
-  //   this.space_ids = await this.fetchMultiple(`team`, this.team_ids, 'space', config);
-  //   this.folder_ids = await this.fetchMultiple(`space`, this.space_ids, 'folder', config);
-  //   const lists_ids = await this.fetchMultiple(`folder`, this.folder_ids, 'list', config);
-  //   const folderless_list_ids = await this.fetchMultiple(`space`, this.space_ids, 'list', config);
-  //   this.list_ids = lists_ids.concat(folderless_list_ids)
-  // }
-
-  static uniqueById(array: IClickUpItem[]) {
-    const seen = new Set();
-    return array.filter(item => {
-      const isDuplicate = seen.has(item.id);
-      seen.add(item.id);
-      return !isDuplicate;
-    });
-  };
+  
 
   protected getConfig(): IRequestConfig {
     return {
@@ -41,10 +24,7 @@ export class ClickUpApi extends HttpClient {
     };
   }
 
-  // protected async fetchIds(path: string, config: IRequestConfig): Promise<string[]> {
-  //   const items = await this.getItems(`/${path}`, config);
-  //   return this.getIds(items[`${path}s`]);
-  // }
+  
 
 
   // return without await
@@ -74,45 +54,5 @@ export class ClickUpApi extends HttpClient {
     });
   }
 
-  protected async getIds(items: IClickUpItem[]) {
-    return items.map((item: IClickUpItem) => item.id);
-  }
-
-  public async sendGetRequest(path: ClickUpApiPath): Promise<any> {
-    if (!this.lists || !this.list_ids) throw new Error(`Ids not properly initialized`);
-    switch (path) {
-      case ClickUpApiPath.Lists:
-        return this.lists;
-        
-      case ClickUpApiPath.Members:
-        return ClickUpApi.uniqueById((await Promise.all((this.list_ids as string[]).map(async (list_id) => {
-          const request_path = `/list/${list_id}/` + String(path);
-          return  (await this.getItems(request_path, this.getConfig()))[`${String(path)}s`];
-        }))).flat());
-
-      case ClickUpApiPath.Tasks:
-        return (await Promise.all((this.list_ids as string[]).map(async (list_id) => {
-          const request_path = `/list/${list_id}/` + String(path);
-          return  (await this.getItems(request_path, this.getConfig()))[`${String(path)}s`];
-        }))).flat();
-      
-    }
-  }
-
-  public async sendGetTaskRequest(queryParams?: IClickUpQueryParams): Promise<IClickUpTaskResponse> {
-    if (!this.list_ids) throw new Error(`Ids not properly initialized`);
-    
-    let isLastPage = true
-    const allItems = (await Promise.all((this.list_ids as string[]).map(async (list_id) => {
-      const request_path = `/list/${list_id}/task`;
-      const items = await this.getItems(request_path, this.getConfig(), queryParams);
-      isLastPage = isLastPage && items.last_page;
-      return  items.tasks;
-    }))).flat();
-    return {
-      tasks: allItems,
-      lastPage: isLastPage,
-    }
-  }
   
 }
--- src/clickup/service.ts
+++ src/clickup/service.ts
@@ -1,58 +1,171 @@
 import { IMerjoonProjects, IMerjoonService, IMerjoonTasks, IMerjoonUsers } from '../common/types';
-import { IClickUpMember, IClickUpList, IClickUpTask, ClickUpApiPath, IClickUpTaskResponse } from './types';
+import { IClickUpMember, IClickUpList, IClickUpTask, ClickUpApiPath, IClickUpTaskResponse, IClickUpItem } from './types';
 import { ClickUpTransformer } from './transformer';
 import { ClickUpApi } from './api';
 
 export class ClickUpService implements IMerjoonService {
 
+  protected teamIds: string[] | undefined;
+  protected spaceIds: string[] | undefined;
+  protected folderIds: string[] | undefined;
+  protected listIds: string[] | undefined;
+
   constructor(public readonly api: ClickUpApi, public readonly transformer: ClickUpTransformer) {
   }
 
-  protected async getAllRecords<T>(path: ClickUpApiPath) {
-    const records: T[] = await this.api.sendGetRequest(path);
-    return records;
+  static uniqueById(array: IClickUpItem[]) {
+    const seen = new Set();
+    return array.filter(item => {
+      const isDuplicate = seen.has(item.id);
+      seen.add(item.id);
+      return !isDuplicate;
+    });
+  };
+
+  protected getIds(items: IClickUpItem[]) {
+    return items.map((item: IClickUpItem) => item.id);
   }
 
-  protected async* getAllTasksIterator(): AsyncGenerator<IClickUpTaskResponse> {
-    let lastPage = false;
-    let currentPage = 0;
-    do {
-      try {
-        const data: IClickUpTaskResponse = await this.api.sendGetTaskRequest({
-          page: currentPage
-        });
-        yield data;
-        lastPage = data.lastPage;
-        currentPage++;
-        // eslint-disable-next-line  @typescript-eslint/no-explicit-any
-      } catch (e: any) {
-        throw new Error(e.message);
-      }
-    } while (!lastPage)
+  // protected async getAllRecords<T>(path: ClickUpApiPath) {
+  //   const records: T[] = await this.api.sendGetRequest(path);
+  //   return records;
+  // }
+
+  // protected async* getAllTasksIterator(): AsyncGenerator<IClickUpTaskResponse> {
+  //   let lastPage = false;
+  //   let currentPage = 0;
+  //   do {
+  //     try {
+  //       const data: IClickUpTaskResponse = await this.api.sendGetTaskRequest({
+  //         page: currentPage
+  //       });
+  //       yield data;
+  //       lastPage = data.lastPage;
+  //       currentPage++;
+  //       // eslint-disable-next-line  @typescript-eslint/no-explicit-any
+  //     } catch (e: any) {
+  //       throw new Error(e.message);
+  //     }
+  //   } while (!lastPage)
+  // }
+
+  // protected async getAllTasks(): Promise<IClickUpTask[]> {
+  //   const iterator: AsyncGenerator<IClickUpTaskResponse> = this.getAllTasksIterator();
+  //   let records: IClickUpTask[] = [];
+
+  //   for await (const nextChunk of iterator) {
+  //     records = records.concat(nextChunk.tasks);
+  //   }
+
+  //   return records;
+  // }
+
+  public async sendGetRequest(path: ClickUpApiPath): Promise<any> {
+    // if (!this.lists || !this.list_ids) throw new Error(`Ids not properly initialized`);
+    // switch (path) {
+    //   case ClickUpApiPath.Lists:
+    //     return this.lists;
+        
+    //   case ClickUpApiPath.Members:
+    //     return ClickUpApi.uniqueById((await Promise.all((this.list_ids as string[]).map(async (list_id) => {
+    //       const request_path = `/list/${list_id}/` + String(path);
+    //       return  (await this.getItems(request_path, this.getConfig()))[`${String(path)}s`];
+    //     }))).flat());
+
+    //   case ClickUpApiPath.Tasks:
+    //     return (await Promise.all((this.list_ids as string[]).map(async (list_id) => {
+    //       const request_path = `/list/${list_id}/` + String(path);
+    //       return  (await this.getItems(request_path, this.getConfig()))[`${String(path)}s`];
+    //     }))).flat();
+      
+    // }
   }
 
-  protected async getAllTasks(): Promise<IClickUpTask[]> {
-    const iterator: AsyncGenerator<IClickUpTaskResponse> = this.getAllTasksIterator();
-    let records: IClickUpTask[] = [];
+  public async sendGetTaskRequest(queryParams?: IClickUpQueryParams): Promise<IClickUpTaskResponse> {
+  //   if (!this.list_ids) throw new Error(`Ids not properly initialized`);
+    
+  //   let isLastPage = true
+  //   const allItems = (await Promise.all((this.list_ids as string[]).map(async (list_id) => {
+  //     const request_path = `/list/${list_id}/task`;
+  //     const items = await this.getItems(request_path, this.getConfig(), queryParams);
+  //     isLastPage = isLastPage && items.last_page;
+  //     return  items.tasks;
+  //   }))).flat();
+  //   return {
+  //     tasks: allItems,
+  //     lastPage: isLastPage,
+  //   }
+  }
 
-    for await (const nextChunk of iterator) {
-      records = records.concat(nextChunk.tasks);
+  protected async getItems(name: string, ids: string[]) {
+    let items: any[] = [];
+    for (const id of ids) {
+      const chunk = await (this.api as any)[`get${name.charAt(0).toUpperCase() + name.slice(1)}s`](id);
+      items = items.concat(chunk[`${name}s`]);
     }
-
-    return records;
+    return items;
   }
 
-  public async getProjects(): Promise<IMerjoonProjects> {
-    const projects = await this.getAllRecords<IClickUpList>(ClickUpApiPath.Lists);
-    return this.transformer.transformLists(projects);
+  protected async getTeams() {
+    const teams = this.api.getTeams();
+    return teams['teams'];
+  }
+
+  protected async getSpaces() {
+    if (!this.teamIds) {
+      throw new Error('Team IDs not found');
+    }
+    return this.getItems('space', this.teamIds);
+  }
+
+  protected async getFolders() {
+    if (!this.spaceIds) {
+      throw new Error('Space IDs not found');
+    }
+    return this.getItems('folder', this.spaceIds);
+  }
+
+  protected async getLists() {
+    if (!this.folderIds) {
+      throw new Error('Folder IDs not found');
+    }
+    return this.getItems('list', this.folderIds);
+  }
+
+  protected async getFolderlessLists() {
+    if (!this.spaceIds) {
+      throw new Error('SpaceIDs not found');
+    };
+    return this.getItems('folderless', this.spaceIds);
   }
 
   public async getUsers(): Promise<IMerjoonUsers> {
-    const people = await this.getAllRecords<IClickUpMember>(ClickUpApiPath.Members);
-    return this.transformer.transformMembers(people);
+    const teams = await this.getTeams();
+    this.teamIds = this.getIds(teams);
+    const members = this.getMembers();
+    return this.transformer.transformMembers(members);
+  }
+
+  public async getProjects(): Promise<IMerjoonProjects> {
+    if (!this.teamIds) {
+      throw new Error('Space IDs not found');
+    }
+    const spaces = await this.getSpaces();
+    this.spaceIds = this.getIds(spaces);
+    const folders = await this.getFolders();
+    this.folderIds = this.getIds(folders);
+    const lists = await this.getLists();
+    const folderlessLists = await this.getFolderlessLists();
+    lists.push(...folderlessLists);
+    this.listIds = this.getIds(lists);
+    return this.transformer.transformLists(lists);
+
   }
 
   public async getTasks(): Promise<IMerjoonTasks> {
+    if (!this.listIds) {
+      throw new Error('List IDs not found');
+    }
     const tasks = await this.getAllTasks();
     return this.transformer.transformTasks(tasks);
   }
