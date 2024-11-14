c00f55df9a425c1fcdbd4d3f5bd0a3355ef41337
Maga <magaharutyunyan969@gmail.com>
2024-11-14 14:23:42 +0400

    MRJ-83 - Updates CI/CD - Run tests

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,6 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   setupFiles: ["<rootDir>/setup-tests.ts"],
-  collectCoverage: true,
   coverageDirectory: 'coverage',
   coverageReporters: ['text'],
 };
