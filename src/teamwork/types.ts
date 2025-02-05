9ff36fc37f610b1c28bcd922965827eee64ad04b
RafoHakobyan <rafo.hakobian@gmail.com>
2025-02-05 17:25:28 +0400

    MRJ-74 - Teamwork Integration/V3

--- src/teamwork/types.ts
+++ src/teamwork/types.ts
@@ -1,10 +1,12 @@
-import https from 'https';
-
 export interface ITeamworkConfig {
   token: string;
   password: string;
   subdomain: string;
-  httpsAgent?: https.Agent;
+  httpsAgent?:ITeamworkConfigHttpsAgent ;
+}
+
+export interface ITeamworkConfigHttpsAgent  {
+  maxSockets?: number;
 }
 
 export interface ITeamworkQueryParams {
@@ -22,30 +24,31 @@ export interface ITeamworkPeople {
   'id': number;
   'firstName': string;
   'lastName': string;
-  'full-name': string;
-  'email-address': string;
-  'created-at': string;
-  'last-changed-on': string;
+  'fullName': string;
+  'email': string;
+  'createdAt': string;
+  'lastChangedOn': string;
 }
 
 export interface ITeamworkProject {
   'id': string;
   'name': string;
   'description': string;
-  'created-on': string;
-  'last-changed-on': string;
+  'createdOn': string;
+  'lastChangedOn': string;
 }
 
 export interface ITeamworkTask {
   'id': number;
   'boardColumn': ITeamworkTaskBoardColumn;
   'content': string;
-  'responsible-party-ids'?: string;
+  'responsiblePartyIds'?: string;
   'description': string;
-  'created-on': string;
-  'last-changed-on': string;
+  'createdOn': string;
+  'lastChangedOn': string;
   'assignees': ITeamworkTaskAssignee[];
-  'projects'?: ITeamworkTaskProjects[];
+  'projects'?: string;
+  'projectId'?: string;
 
 }
 export interface ITeamworkTaskBoardColumn {
@@ -58,11 +61,4 @@ export interface ITeamworkItem {
 }
 export interface ITeamworkTaskAssignee {
   'id'?: string;
-}
-export interface ITeamworkTaskProjects{
-  'id'?:string;
-}
-
-export interface ITeamworkItem {
-  id: string;
 }
