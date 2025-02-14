8124184a8eb50f1fd6aefd2f1e4035056910a205
Sam <sam.soghomonyan@bk.ru>
2025-02-14 13:59:15 +0400

    MRJ-10 - GitLab Integration - API Client

--- src/teamwork/service.ts
+++ src/teamwork/service.ts
@@ -6,13 +6,10 @@ import { TEAMWORK_PATHS } from './consts';
 
 export class TeamworkService implements IMerjoonService {
   protected projectIds?: number[];
-
   static mapIds(items: ITeamworkItem[]) {
     return items.map((item: ITeamworkItem) => item.id);
   }
-
   constructor(public readonly api: TeamworkApi, public readonly transformer: TeamworkTransformer) {}
-
   protected async* getAllRecordsIterator(path: TeamworkApiPath, pageSize = 50) {
     let shouldStop = false;
     let currentPage = 1;
@@ -21,9 +18,7 @@ export class TeamworkService implements IMerjoonService {
         page: currentPage,
         pageSize,
       });
-
       yield data.projects || data.people || data.tasks;
-
       shouldStop = !data.meta.page.hasMore;
       currentPage++;
     } while (!shouldStop);
