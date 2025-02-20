7d5172106b8afac8803a1289ac6b470a0d697813
RafoHakobyan <rafo.hakobian@gmail.com>
2025-02-20 23:49:44 +0400

    MRJ-121 - Modify Es lint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,15 +1,11 @@
 import tseslint from 'typescript-eslint';
 import eslint from '@eslint/js';
-import eslintPluginImport from 'eslint-plugin-import';
 
 export default tseslint.config(
   eslint.configs.recommended,
   ...tseslint.configs.strict,
   ...tseslint.configs.stylisticTypeChecked,
   {
-    plugins: {
-      import: eslintPluginImport,
-    },
     rules: {
       semi: ['error', 'always'],
       quotes: ['error', 'single', {
@@ -25,27 +21,11 @@ export default tseslint.config(
       'quote-props': ['error', 'as-needed'],
       'nonblock-statement-body-position': ['error', 'below'],
       'object-curly-spacing': ['error', 'always'],
-      'no-trailing-spaces': 'error',
       'brace-style': ['error', '1tbs'],
       'space-infix-ops': ['error', {
         int32Hint: false
       }],
-      'key-spacing': ['error', {
-        afterColon: true
-      }],
-      'keyword-spacing': ['error', {
-        before: true, after: true
-      }],
-      'space-before-blocks': ['error', 'always'],
       curly: ['error'],
-      'import/no-mutable-exports': 'error',
-      'import/order': [
-        'error',
-        {
-          groups: [['builtin', 'external', 'internal']],
-          'newlines-between': 'always',
-        },
-      ],
       'object-curly-newline': [
         'error',
         {
@@ -53,6 +33,7 @@ export default tseslint.config(
           ImportDeclaration: 'always',
         },
       ],
+
     },
     languageOptions: {
       parserOptions: {
