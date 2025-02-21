2c69e42788cde9b397fed4704757f620b204103f
Sam Soghomonyan <sam.soghomonyan@bk.ru>
2025-02-21 09:25:52 +0400

    MRJ-118 - GitLab Integration - Service

--- src/gitlab/__tests__/e2e.gitlab.service.tese.ts
+++ src/gitlab/__tests__/e2e.gitlab.service.tese.ts
@@ -1,4 +1,4 @@
-import { IMerjoonProjects } from '../../common/types';
+import { IMerjoonProjects, IMerjoonTasks, IMerjoonUsers } from '../../common/types';
 import {GitLabService} from '../service';
 import {getGitLabService} from '../gitlab_service';
 import {ID_REGEX} from '../../utils/regex';
@@ -8,55 +8,54 @@ describe('GitLab Service', () => {
     service = getGitLabService();
   });
   describe('getUsers', () =>{
-    // it('should return a valid user structure',async () => {
-    //   await service.getGroups();
-    //   const users:IMerjoonUsers = await service.getUsers();
-    //   expect(Object.keys(users[0])).toEqual(expect.objectContaining([
-    //     'id',
-    //     'remote_id',
-    //     'name',
-    //     'remote_created_at'
-    //   ]));
-    //   expect(users[0]).toEqual({
-    //     id:expect.stringMatching(ID_REGEX),
-    //     remote_id:expect.any(Number),
-    //     name: expect.any(String),
-    //     created_at:expect.any(Number),
-    //     modified_at:expect.any(Number),
-    //     remote_created_at:expect.any(String),
-    //   });
-    // },);
+    it('should return a valid user structure',async () => {
+      const users:IMerjoonUsers = await service.getUsers();
+      expect(Object.keys(users[0])).toEqual(expect.objectContaining([
+        'id',
+        'remote_id',
+        'name',
+        'remote_created_at'
+      ]));
+      expect(users[0]).toEqual({
+        id:expect.stringMatching(ID_REGEX),
+        remote_id:expect.any(Number),
+        name: expect.any(String),
+        created_at:expect.any(Number),
+        modified_at:expect.any(Number),
+        remote_created_at:expect.any(String),
+      });
+    },);
   },);
-  // describe('getTasks', () => {
-  //   it('should return a valid Tasks structure', async() => {
-  //     const tasks:IMerjoonTasks = await service.getTasks();
-  //     expect(Object.keys(tasks[0])).toEqual(expect.arrayContaining([
-  //       'id',
-  //       'remote_id',
-  //       'name',
-  //       'assignees',
-  //       'status',
-  //       'description',
-  //       'remote_created_at',
-  //       'remote_modified_at',
-  //       'ticket_url',
-  //     ]));
-  //     expect(tasks[0]).toEqual({
-  //       id:expect.stringMatching(ID_REGEX),
-  //       remote_id:expect.any(Number),
-  //       name:expect.any(String),
-  //       assignees: expect.arrayContaining([expect.stringMatching(ID_REGEX)]),
-  //       modified_at:expect.any(Number),
-  //       status: expect.any(String),
-  //       description: expect.any(String),
-  //       remote_created_at: expect.any(String),
-  //       remote_modified_at: expect.any(String),
-  //       ticket_url: expect.any(String),
-  //       created_at:expect.any(Number),
-  //       projects: expect.arrayContaining([expect.stringMatching(ID_REGEX)]),
-  //     });
-  //   },20000);
-  // });
+  describe('getTasks', () => {
+    it('should return a valid Tasks structure', async() => {
+      const tasks:IMerjoonTasks = await service.getTasks();
+      expect(Object.keys(tasks[0])).toEqual(expect.arrayContaining([
+        'id',
+        'remote_id',
+        'name',
+        'assignees',
+        'status',
+        'description',
+        'remote_created_at',
+        'remote_modified_at',
+        'ticket_url',
+      ]));
+      expect(tasks[0]).toEqual({
+        id:expect.stringMatching(ID_REGEX),
+        remote_id:expect.any(Number),
+        name:expect.any(String),
+        assignees: expect.arrayContaining([expect.stringMatching(ID_REGEX)]),
+        modified_at:expect.any(Number),
+        status: expect.any(String),
+        description: expect.any(String),
+        remote_created_at: expect.any(String),
+        remote_modified_at: expect.any(String),
+        ticket_url: expect.any(String),
+        created_at:expect.any(Number),
+        projects: expect.arrayContaining([expect.stringMatching(ID_REGEX)]),
+      });
+    },20000);
+  });
   describe('getProjects', () => {
     it('should return a valid projects structure', async() => {
       const projects:IMerjoonProjects = await service.getProjects();
--- src/gitlab/service.ts
+++ src/gitlab/service.ts
@@ -1,32 +1,25 @@
 import { IMerjoonProjects, IMerjoonService, IMerjoonTasks, IMerjoonUsers } from '../common/types';
 import { GitLab } from './api';
 import { GitLabTransformer } from './transformer';
+import {IGitLabGroup} from './types';
 
 export class GitLabService implements IMerjoonService {
-  groupsIDs?: string[];
+  protected groupsIDs?: string[];
+  constructor(public readonly api: GitLab, public readonly transformer: GitLabTransformer) {
+  }
   public async init(){
     return;
   }
-  constructor(public readonly api: GitLab, public readonly transformer: GitLabTransformer) {
-    this.groupsIDs = [];
+  static mapIds(item:IGitLabGroup[]){
+    return item.map((item:IGitLabGroup) => item.id);
   }
-
   public async getProjects(): Promise<IMerjoonProjects> {
     const projects = await this.api.getAllProjects();
     return this.transformer.transformProjects(projects);
   }
-
-  public async getGroups() {
-    const groups = await this.api.getAllGroups();
-    if(groups?.length > 0){
-      this.groupsIDs = groups.map(group => group.id);
-    }
-  }
-
   public async getUsers(): Promise<IMerjoonUsers> {
-    if (!this.groupsIDs?.length) {
-      return [];
-    }
+    const groups = await this.api.getAllGroups();
+    this.groupsIDs = GitLabService.mapIds(groups);
     const members = await Promise.all(
       this.groupsIDs.map(groupId => this.api.getAllMembersByGroupId(groupId))
     );
