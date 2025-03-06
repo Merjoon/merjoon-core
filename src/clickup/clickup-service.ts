e48a30046e28ed47a3e8f132207e3822664f149a
Sam Soghomonyan <sam.soghomonyan@bk.ru>
2025-03-06 18:34:32 +0400

    MRJ-94 - HTTP Agent Generalization

--- src/clickup/clickup-service.ts
+++ src/clickup/clickup-service.ts
@@ -18,6 +18,7 @@ export function getClickUpService(): ClickUpService {
     config.httpsAgent = {
       maxSockets: Number(CLICKUP_HTTPS_AGENT_MAX_SOCKETS),
     };
+    config.useHttpsAgent = Boolean(config.useHttpsAgent);
   }
 
   const api: ClickUpApi = new ClickUpApi(config);
