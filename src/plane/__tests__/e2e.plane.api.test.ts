b816f65b6c88c05ebe4b09ab4692b05f74905d79
KarenSargsyan90 <karensargsyan471@gmail.com>
2025-03-19 19:01:54 +0400

    MRJ-91 - Plane Integration API Client-Project

--- src/plane/__tests__/e2e.plane.api.test.ts
+++ src/plane/__tests__/e2e.plane.api.test.ts
@@ -2,41 +2,40 @@ jest.setTimeout(15000);
 import { PlaneApi } from '../api';
 import { IPlaneConfig } from '../types';
 
-
 const apiKey = process.env.PLANE_API_KEY;
 const workspaceSlug = process.env.WORKSPACE_SLUG;
 
 if (!apiKey) {
-    throw new Error('Plane token is not set in the environment variables');
+  throw new Error('Plane token is not set in the environment variables');
 }
 if (!workspaceSlug) {
-    throw new Error('Workspace slug is not set in the environment variables');
+  throw new Error('Workspace slug is not set in the environment variables');
 }
 
 describe('Plane API', () => {
-    let plane: PlaneApi;
-    let config: IPlaneConfig;
+  let plane: PlaneApi;
+  let config: IPlaneConfig;
 
-    beforeEach(async () => {
-        config = {
-            apiKey: apiKey,
-            workspaceSlug: workspaceSlug,
-        };
-        plane = new PlaneApi(config);
-    });
+  beforeEach(async () => {
+    config = {
+      apiKey: apiKey,
+      workspaceSlug: workspaceSlug,
+    };
+    plane = new PlaneApi(config);
+  });
 
-    describe('getAllProjects', () => {
-        it('should fetch all projects', async () => {
-            const projects = await plane.getAllProjects();
-            expect(projects[0]).toEqual(
-                expect.objectContaining({
-                    id: expect.any(String),
-                    name: expect.any(String),
-                    description: expect.any(String),
-                    created_at: expect.any(String),
-                    updated_at: expect.any(String),
-                }),
-            );
-        });
+  describe('getAllProjects', () => {
+    it('should fetch all projects', async () => {
+      const projects = await plane.getAllProjects();
+      expect(projects[0]).toEqual(
+        expect.objectContaining({
+          id: expect.any(String),
+          name: expect.any(String),
+          description: expect.any(String),
+          created_at: expect.any(String),
+          updated_at: expect.any(String),
+        }),
+      );
     });
+  });
 });
