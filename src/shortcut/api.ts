139adda7d398604dd3ef5f4ec2936a1ad873ee2d
Sam Soghomonyan <sam.soghomonyan@bk.ru>
2025-04-03 18:29:20 +0400

    MRJ-155 - Test Failures Related to Shortcut Integration

--- src/shortcut/api.ts
+++ src/shortcut/api.ts
@@ -51,7 +51,7 @@ export class ShortcutApi extends HttpClient {
       records = records.concat(nextChunk);
     }
 
-    return records;
+    return records.reverse();
   }
 
   public async getStories(queryParamsObject: object): Promise<IShortcutStoriesResponse> {
