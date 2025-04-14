f7e9fa5b4d6c044df71e69d7809e6dbe3a2f69a5
Sam Soghomonyan <sam.soghomonyan@bk.ru>
2025-04-14 13:45:40 +0400

    MRJ-152 - HTTP Client Test

--- src/common/__tests__/e2e.http-client.test.ts
+++ src/common/__tests__/e2e.http-client.test.ts
@@ -18,112 +18,108 @@ describe('HttpClient E2E Test', () => {
     await httpClientServer.stop();
     jest.clearAllMocks();
   });
-
-  it('should handle connections with HTTP agent', async () => {
-    const config: IMerjoonApiConfig = {
-      baseURL: httpClientServer.baseUrl,
-      httpAgent: {
-        maxSockets: 10,
-        keepAlive: true,
-      },
-    };
-    httpClient = new HttpClient(config);
-    const requests = Array.from(
-      {
-        length: 100,
-      },
-      () =>
-        httpClient.get({
-          path: '',
-        }),
-    );
-    await Promise.all(requests);
-    expect(httpClientServer.maxConnections).toEqual(10);
-  });
-
-  it('should handle connections without HTTP agent', async () => {
-    const config: IMerjoonApiConfig = {
-      baseURL: httpClientServer.baseUrl,
-    };
-    httpClient = new HttpClient(config);
-    const requests = Array.from(
-      {
-        length: 100,
-      },
-      () =>
-        httpClient.get({
-          path: '',
-        }),
-    );
-    await Promise.all(requests);
-    expect(httpClientServer.maxConnections).toEqual(100);
-  });
-
-  it('should return correct response structure for successful request', async () => {
-    const config: IMerjoonApiConfig = {
-      baseURL: httpClientServer.baseUrl,
-    };
-    httpClient = new HttpClient(config);
-
-    const response = await httpClient.get<{ message: string }>({
-      path: '',
-    });
-    expect(response).toHaveProperty('data');
-    expect(response.data).toEqual({
-      message: 'Hello, world!',
-    });
-    expect(response).toHaveProperty('status');
-    expect(response.status).toBe(200);
-    expect(response).toHaveProperty('headers');
-    expect(response.headers).toHaveProperty('content-type', 'application/json');
-  });
-
-  it('should return correct response structure for failed request', async () => {
-    expect.assertions(5);
-    const config: IMerjoonApiConfig = {
-      baseURL: httpClientServer.baseUrl,
-    };
-    httpClient = new HttpClient(config);
-
-    try {
-      await httpClient.get<{ message: string }>({
-        path: 'user',
-      });
-    } catch (error) {
-      const typedError = error as {
-        status: number;
-        data: { message: string };
-        headers: Record<string, string>;
+  describe('HTTP agent e2e test', () => {
+    it('should handle connections with HTTP agent', async () => {
+      const config: IMerjoonApiConfig = {
+        baseURL: httpClientServer.baseUrl,
+        httpAgent: {
+          maxSockets: 10,
+          keepAlive: true,
+        },
       };
+      httpClient = new HttpClient(config);
+      const requests = Array.from(
+        {
+          length: 100,
+        },
+        () =>
+          httpClient.get({
+            path: '',
+          }),
+      );
+      await Promise.all(requests);
+      expect(httpClientServer.maxConnections).toEqual(10);
+    });
 
-      expect(typedError).toHaveProperty('status', 404);
-      expect(typedError).toHaveProperty('data');
-      expect(typedError.data).toHaveProperty('message', 'Page not Found');
-      expect(typedError).toHaveProperty('headers');
-      expect(typedError.headers).toHaveProperty('content-type', 'application/json');
-    }
+    it('should handle connections without HTTP agent', async () => {
+      const config: IMerjoonApiConfig = {
+        baseURL: httpClientServer.baseUrl,
+      };
+      httpClient = new HttpClient(config);
+      const requests = Array.from(
+        {
+          length: 100,
+        },
+        () =>
+          httpClient.get({
+            path: '',
+          }),
+      );
+      await Promise.all(requests);
+      expect(httpClientServer.maxConnections).toEqual(100);
+    });
   });
+  describe('SendRequest e2e test', () => {
+    it('should return correct response structure for successful request', async () => {
+      const config: IMerjoonApiConfig = {
+        baseURL: httpClientServer.baseUrl,
+      };
+      httpClient = new HttpClient(config);
 
-  it('should throw original error when error is not AxiosError', async () => {
-    expect.assertions(1);
-    const config: IMerjoonApiConfig = {
-      baseURL: httpClientServer.baseUrl,
-    };
-    httpClient = new HttpClient(config);
-
-    const notAxiosError = new Error('Network failure');
-
-    jest.spyOn(httpClient, 'get').mockRejectedValueOnce(notAxiosError);
-
-    try {
-      await httpClient.get({
+      const response = await httpClient.get<{ message: string }>({
         path: '',
       });
-    } catch (error) {
-      if (error instanceof Error) {
-        expect(error.message).toBe('Network failure');
+      expect(response).toHaveProperty('data');
+      expect(response.data).toEqual({
+        message: 'Hello, world!',
+      });
+      expect(response).toHaveProperty('status');
+      expect(response.status).toBe(200);
+      expect(response).toHaveProperty('headers');
+      expect(response.headers).toHaveProperty('content-type', 'application/json');
+    });
+
+    it('should return correct response structure for failed request', async () => {
+      expect.assertions(5);
+      const config: IMerjoonApiConfig = {
+        baseURL: httpClientServer.baseUrl,
+      };
+      httpClient = new HttpClient(config);
+
+      try {
+        await httpClient.get<{ message: string }>({
+          path: 'users',
+        });
+      } catch (error) {
+        expect(error).toHaveProperty('status', 404);
+        expect(error).toHaveProperty('data');
+        expect(error).toHaveProperty('data.message', 'Page not Found');
+        expect(error).toHaveProperty('headers');
+        expect(error).toHaveProperty('headers.content-type', 'application/json');
       }
-    }
+    });
+
+    it('should throw original error when error is not AxiosError', async () => {
+      expect.assertions(1);
+      const config: IMerjoonApiConfig = {
+        baseURL: httpClientServer.baseUrl,
+      };
+      httpClient = new HttpClient(config);
+
+      const notAxiosError = new Error('unknown error');
+
+      jest.spyOn(httpClient, 'get').mockRejectedValueOnce(notAxiosError);
+
+      try {
+        await httpClient.get({
+          path: '',
+        });
+      } catch (error) {
+        if (error instanceof Error) {
+          expect(error.message).toBe('unknown error');
+        }
+      }
+    });
   });
 });
 
