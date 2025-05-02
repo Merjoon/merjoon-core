5efccb61a483f183ef79fd55dbf7ff5a3069b4fd
KarenSargsyan90 <karensargsyan471@gmail.com>
2025-05-02 20:57:07 +0400

    MRJ-146 - Plane Integration API Client- Tasks (#118)
    * MRJ-146 - Plane Integration API Client- Tasks
    
    * MRJ-146 - Plane Integration API Client- Tasks
    
    * MRJ-146 - Plane Integration API Client- Tasks
    
    * MRJ-146 - Plane Integration API Client- Tasks
    
    * MRJ-146 - Plane Integration API Client- Tasks
    
    * MRJ-146 - Plane Integration API Client- Tasks
    
    * MRJ-146 - Plane Integration API Client- Tasks
    
    * MRJ-146 - Plane Integration API Client- Tasks
    
    * MRJ-146 - Plane Integration API Client- Tasks
    
    * MRJ-146 - Plane Integration API Client- Tasks
    
    * MRJ-146 - Plane Integration API Client- Tasks
    
    * MRJ-146 - Plane Integration API Client- Tasks
    
    * MRJ-146 - Plane Integration API Client- Tasks
    
    * MRJ-146 - Plane Integration API Client- Tasks
    
    * MRJ-146 - Plane Integration API Client- Tasks
    
    * MRJ-146 - Plane Integration API Client- Tasks
    
    * MRJ-146 - Plane Integration API Client- Tasks
    
    * MRJ-146 - Plane Integration API Client- Tasks
    
    * MRJ-146 - Plane Integration API Client- Tasks
    
    * MRJ-146 - Plane Integration API Client- Tasks
    
    * MRJ-146 - Plane Integration API Client- Tasks
    
    * MRJ-146 - Plane Integration API Client- Tasks
    
    * MRJ-146 - Plane Integration API Client- Tasks
    
    * MRJ-146 - Plane Integration API Client- Tasks
    
    * MRJ-146 - Plane Integration API Client- Tasks
    
    * MRJ-146 - Plane Integration API Client- Tasks
    
    * MRJ-146 - Plane Integration API Client- Tasks
    
    * MRJ-146 - Plane Integration API Client- Tasks

--- src/plane/__tests__/e2e.plane.api.test.ts
+++ src/plane/__tests__/e2e.plane.api.test.ts
@@ -16,17 +16,45 @@ describe('Plane API', () => {
   let plane: PlaneApi;
   let config: IPlaneConfig;
 
-  beforeEach(async () => {
+  beforeEach(() => {
     config = {
-      apiKey: apiKey,
-      workspaceSlug: workspaceSlug,
+      apiKey,
+      workspaceSlug,
+      limit: 3,
     };
     plane = new PlaneApi(config);
   });
 
-  describe('getAllProjects', () => {
-    it('should fetch all projects', async () => {
-      const projects = await plane.getAllProjects();
+  afterEach(async () => {
+    jest.restoreAllMocks();
+  });
+
+  describe('Plane Issues Pagination', () => {
+    describe('getAllIssues', () => {
+      it('should iterate over all issues of a project and return unique IDs', async () => {
+        const getIssuesSpy = jest.spyOn(plane, 'getIssuesByProjectId');
+
+        const projects = await plane.getProjects();
+        const projectId = projects[0]?.id;
+
+        const issues = await plane.getAllIssues(projectId);
+        const itemsCount = issues.length;
+        const totalPagesCalledCount = Math.ceil(itemsCount / plane.limit);
+
+        const issueIds = issues.map((issue) => issue.id);
+        const uniqueIds = new Set(issueIds);
+
+        expect(issueIds.length).toBe(uniqueIds.size);
+        expect(getIssuesSpy).toHaveBeenCalledTimes(totalPagesCalledCount);
+        expect(totalPagesCalledCount).toBeGreaterThan(1);
+      });
+    });
+  });
+
+  describe('getProjects', () => {
+    it('should fetch projects', async () => {
+      const projects = await plane.getProjects();
+      expect(projects.length).toBeGreaterThan(0);
       expect(projects[0]).toEqual(
         expect.objectContaining({
           id: expect.any(String),
@@ -34,6 +62,32 @@ describe('Plane API', () => {
           description: expect.any(String),
           created_at: expect.any(String),
           updated_at: expect.any(String),
+        }),
+      );
+    });
+  });
+
+  describe('getAllIssues', () => {
+    it('should fetch all issues for first project', async () => {
+      const projects = await plane.getProjects();
+      const projectId = projects[0].id;
+      const issues = await plane.getAllIssues(projectId);
+
+      expect(issues[0].assignees.length).toBeGreaterThan(0);
+      expect(issues.length).toBeGreaterThan(0);
+      expect(issues[0]).toEqual(
+        expect.objectContaining({
+          id: expect.any(String),
+          name: expect.any(String),
+          description_stripped: expect.any(String),
+          assignees: expect.arrayContaining([expect.any(String)]),
+          project: expect.any(String),
+          created_at: expect.any(String),
+          updated_at: expect.any(String),
+          state: expect.objectContaining({
+            id: expect.any(String),
+            name: expect.any(String),
+          }),
         }),
       );
     });
