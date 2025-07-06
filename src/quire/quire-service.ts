3551b5dec2c22afc107d47059c1a442b7ee3bad9
RafoHakobyan <rafo.hakobian@gmail.com>
2025-07-06 18:09:02 +0400

    MRJ-168 - Quire Integration - Service

--- src/quire/quire-service.ts
+++ src/quire/quire-service.ts
@@ -4,7 +4,8 @@ import { QuireApi } from './api';
 import { QuireTransformer } from './transformer';
 
 export async function getQuireService(): Promise<QuireService> {
-  const { QUIRE_REFRESH_TOKEN, QUIRE_CLIENT_ID, QUIRE_CLIENT_SECRET } = process.env;
+  const { QUIRE_REFRESH_TOKEN, QUIRE_CLIENT_ID, QUIRE_CLIENT_SECRET, QUIRE_MAX_SOCKETS } =
+    process.env;
   if (!QUIRE_REFRESH_TOKEN || !QUIRE_CLIENT_ID || !QUIRE_CLIENT_SECRET) {
     throw new Error('Missing necessary environment variables');
   }
@@ -12,6 +13,7 @@ export async function getQuireService(): Promise<QuireService> {
     refreshToken: QUIRE_REFRESH_TOKEN,
     clientId: QUIRE_CLIENT_ID,
     clientSecret: QUIRE_CLIENT_SECRET,
+    maxSockets: Number(QUIRE_MAX_SOCKETS) || 5,
   };
   const api: QuireApi = new QuireApi(config);
   await api.init();
