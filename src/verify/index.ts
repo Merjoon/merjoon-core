e733724ac49b9d0794b8f954e6bd95fe92538013
Davit Hovhannisyan <dav.hovhannisyan.0102@gmail.com>
2025-08-16 18:06:06 +0400

    MRJ-230 - Missing ProjectIds in Todoist Service

--- src/verify/index.ts
+++ src/verify/index.ts
@@ -3,14 +3,13 @@ dotenv.config();
 
 import { getService } from './service-factory';
 import { IntegrationId } from './types';
-import { MerjoonExecutor } from './utils';
+import { fetchEntitiesInOrder } from './utils';
 
 async function main(): Promise<void> {
   const integrationId = process.argv[2] as IntegrationId;
   const { service, dependencies } = await getService(integrationId);
   await service.init();
-  const executor = new MerjoonExecutor(service, integrationId);
-  await executor.fetchEntitiesInOrder(dependencies);
+  await fetchEntitiesInOrder(service, integrationId, dependencies);
 }
 
 main();
