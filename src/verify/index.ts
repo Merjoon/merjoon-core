93e260b79f30dbdcd8dddb62f50b0e4f525b2d2a
Davit Hovhannisyan <dav.hovhannisyan.0102@gmail.com>
2025-08-25 15:07:06 +0400

    MRJ-232 - Write the logic to verify all integrations together

--- src/verify/index.ts
+++ src/verify/index.ts
@@ -5,13 +5,18 @@ import { verifyIntegration } from './utils';
 
 async function main() {
   const integrationId = process.argv[2] as IntegrationId;
-  if (integrationId) {
-    await verifyIntegration(integrationId);
-  } else {
-    const integrations = Object.values(IntegrationId);
-    for (const integrationId of integrations) {
-      await verifyIntegration(integrationId);
-    }
-  }
+  const integrations = integrationId ? [integrationId] : Object.values(IntegrationId);
+
+  await Promise.allSettled(
+    integrations.map(async (id) => {
+      try {
+        await verifyIntegration(id);
+        console.log(`✅ ${id} verified`); // eslint-disable-line no-console
+      } catch (err) {
+        console.error(`❌ ${id} failed`, err); // eslint-disable-line no-console
+      }
+    }),
+  );
 }
+
 main();
