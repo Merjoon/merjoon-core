5de57e3b788b186f7ec85890ae4b844f8bbc7d0c
Davit Hovhannisyan <dav.hovhannisyan.0102@gmail.com>
2025-08-25 14:45:44 +0400

    MRJ-232 - Write the logic to verify all integrations together

--- src/verify/index.ts
+++ src/verify/index.ts
@@ -1,19 +1,17 @@
 import * as dotenv from 'dotenv';
 dotenv.config();
-import { getService } from './service-factory';
 import { IntegrationId } from './types';
-import { saveEntities } from './utils';
+import { verifyIntegration } from './utils';
 
 async function main() {
   const integrationId = process.argv[2] as IntegrationId;
-  const service = await getService(integrationId);
-  await service.init();
-  const users = await service.getUsers();
-  const projects = await service.getProjects();
-  const tasks = await service.getTasks();
-  await saveEntities(integrationId, 'users', users);
-  await saveEntities(integrationId, 'projects', projects);
-  await saveEntities(integrationId, 'tasks', tasks);
+  if (integrationId) {
+    await verifyIntegration(integrationId);
+  } else {
+    const integrations = Object.values(IntegrationId);
+    for (const integrationId of integrations) {
+      await verifyIntegration(integrationId);
+    }
+  }
 }
-
 main();
--- src/verify/utils.ts
+++ src/verify/utils.ts
@@ -1,6 +1,7 @@
 import fs from 'node:fs/promises';
 import { EntityName, IntegrationId } from './types';
 import { IMerjoonEntity } from '../common/types';
+import { getService } from './service-factory';
 
 export async function saveEntities(
   serviceName: IntegrationId,
@@ -12,4 +13,15 @@ export async function saveEntities(
     recursive: true,
   });
   await fs.writeFile(`${folder}/${entityName}.json`, JSON.stringify(payload, null, 2));
+}
+
+export async function verifyIntegration(integrationId: IntegrationId) {
+  const service = await getService(integrationId);
+  await service.init();
+  const users = await service.getUsers();
+  const projects = await service.getProjects();
+  const tasks = await service.getTasks();
+  await saveEntities(integrationId, 'users', users);
+  await saveEntities(integrationId, 'projects', projects);
+  await saveEntities(integrationId, 'tasks', tasks);
 }
